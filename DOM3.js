16a5da3dfb881865aba1c694a11149823367d159
Lakshay Gupta <lkshy196@gmail.com>
2026-03-17 09:56:40 +0530

    17 march

--- DOM3.js
+++ DOM3.js
@@ -1,18 +1,20 @@
-function onClick() {
-    console.log("clicked"); 
-}
-function onMouseOver() {
-    console.log("mouse over"); 
-}
-function onMouseOut() {
-    console.log("mouse out"); 
-}   
-function onMouseDown() {
-    console.log("mouse down"); 
-}
-function onMouseUp() {
-    console.log("mouse up"); 
-}
-function onMouseMove() {
-    console.log("mouse move"); 
-}   
+// function onClick() {
+//     console.log("clicked"); 
+// }
+// function onMouseOver() {
+//     console.log("mouse over"); 
+// }
+// function onMouseOut() {
+//     console.log("mouse out"); 
+// }   
+// function onMouseDown() {
+//     console.log("mouse down"); 
+// }
+// function onMouseUp() {
+//     console.log("mouse up"); 
+// }
+// function onMouseMove() {
+//     console.log("mouse move"); 
+// }   
+
+
